26887d17bc10bdfe99d78ec84d4aef9dd6f38c7e
meowyx <qafka256@gmail.com>
2024-06-25 03:27:52 +0600

    smaller texts

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     <>
       <section className="space-y-6 pb-4 pt-2 md:pb-12 md:mt-10 lg:py-32">
         <div className="container flex flex-col gap-4 text-center">
-          <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-balance">
+          <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold text-balance">
             Hello, I&apos;m Sushmita 👋
           </h1>
           <p className="max-w-[42rem] mx-auto text-muted-foreground sm:text-xl text-balance">
@@ -41,7 +41,7 @@ export default function Home() {
         </div>
       </section>
       <section className="container max-w-4xl py-6 lg:py-10 flex flex-col space-y-6">
-        <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-center">
+        <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold text-center">
           Latest Posts
         </h2>
         <ul className="flex flex-col">
